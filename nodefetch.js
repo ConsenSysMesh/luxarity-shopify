5d362b7d951e7ed82eedce1f05e330cd86ceee6f
sf <>
2018-10-15 09:14:56 -0700

    success sqs dump with 5 soldOrderToMint attributes

--- nodefetch.js
+++ nodefetch.js
@@ -8,26 +8,9 @@ AWS.config.update({region: 'us-west-1'});
 // Create an SQS service object
 var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
-//'{"name" : "Jane Smith", "email" : "jsmith@gmail.com", "password" : "anothergoodpw"}'
 
-
-
-//https://www.npmjs.com/package/node-fetch
 
 /*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
-    .then(res => res.text())
-    .then(body => console.log(body));*/
-
-/*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
-    .then(res => res.json())
-    .then(json => console.log(json));*/
-
-
-//id
-//total_price
-//order_number
-
-fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
     .then(res => res.json())
     .then(json => {
 
@@ -49,22 +32,45 @@ fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18
     	}
     	
     }
-    );
+    );*/
 
 
 
 
-/*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
+fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
     .then(res => res.json())
     .then(json => {
 
+        console.log("looping through orders")
     	for(var i = 0; i < json.orders.length; i++){
-    		console.log("send this order id to sqs queue: "+json.orders[i].id)
-   			try{
-    		sendsqs(json.orders[i].id)
-    		}catch(err){
-    			console.log("err for "+json.orders[i].id+" "+err)
-    		}	
+            
+            //var order = [];
+            //orderid
+            //customer.email
+            //total_price
+            //order_number
+            /*if(json.orders[i].customer){
+            console.log(json.orders[i].customer.email)
+            }else{
+                 console.log(json.orders[i].id)
+            }*/
+
+            if(!(json.orders[i].id && json.orders[i].total_price && json.orders[i].order_number && json.orders[i].customer)){
+                //place holder for std out - possibly dlq
+                console.log("order missing attributes: "+json.orders[i].id+" loop number: "+i)
+            }else{
+                console.log("send this order id to sqs queue: "+json.orders[i].id + " "+json.orders[i].customer.email)
+                try{
+                
+                sendsqs(json.orders[i].id, json.orders[i].total_price, json.orders[i].order_number, json.orders[i].customer.email)
+                
+                }catch(err){
+                    console.log("err for "+json.orders[i].id+" "+err)
+                }
+
+            }
+
+    		
     	}
     	
     }
@@ -72,17 +78,18 @@ fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18
 
 
 
-async function sendsqs(orderid){
+async function sendsqs(orderid, total_price, order_number, customer_email){
 	var params = {
 		 DelaySeconds: 10,
 		 MessageAttributes: {
-		  "test": {
+		  "LuxarityOrder": {
 		    DataType: "String",
-		    StringValue: "sqstest"
+		    StringValue: "Individual Lux Order for SoldOrderToMint endpoint"
 		   }
 		 },
-		 MessageBody: "{ \"orderid\" : \""+orderid+"\" }",
-		 QueueUrl: "https://sqs.us-east-1.amazonaws.com/711302153787/luxarity-orders"
+		 //MessageBody: "{ \"orderid\" : \""+orderid+"\" , \"total_price\" : \""+total_price+"\" , \"order_number\" : \""+order_number+"\" , \"customer_email\" : \""+customer_email+"\"}",
+		 MessageBody: "{ \"tokenURI\" : \""+orderid+"\" , \"saleAmount\" : \""+total_price+"\" , \"redemptionHash\" : \""+order_number+"\" , \"buyerID\" : \""+customer_email+"\" , \"blockchain : \"Rinkeby\"}",
+         QueueUrl: "https://sqs.us-east-1.amazonaws.com/711302153787/luxarity-orders"
 		 //QueueUrl: "https://sqs.us-west-1.amazonaws.com/711302153787/SQS_QUEUE_NAME"
 		};
 
@@ -94,8 +101,25 @@ async function sendsqs(orderid){
 		  }
 		});
 }
-*/
 
 
+//'{"name" : "Jane Smith", "email" : "jsmith@gmail.com", "password" : "anothergoodpw"}'
+
+
+
+//https://www.npmjs.com/package/node-fetch
+
+/*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
+    .then(res => res.text())
+    .then(body => console.log(body));*/
+
+/*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
+    .then(res => res.json())
+    .then(json => console.log(json));*/
+
+
+//id
+//total_price
+//order_number
 
 
