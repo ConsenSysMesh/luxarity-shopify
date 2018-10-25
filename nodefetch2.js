12625e4347d682bc4e43fa81be8f73cd47a7bed0
sf <>
2018-10-25 11:58:28 -0700

    restructured ipfs sqs fetch to support multiple orders

--- nodefetch2.js
+++ nodefetch2.js
@@ -13,54 +13,26 @@ const IPFS = require('ipfs');
 const fs = require('file-system');
 
 
-
-/*fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json')
-    .then(res => res.json())
-    .then(json => {
-
-        console.log(json.orders.length)
-    	for(var i = 0; i < json.orders.length; i++){
-            console.log("###LOOP NUMBER "+i+"###")
-            //console.log("id: "+json.orders[i].id)
-            //console.log("order_number: "+json.orders[i].order_number)
-            //console.log("total price: "+json.orders[i].total_price)
-            //console.log("email: "+json.orders[i].email)
-            //console.log("fulfillment_status: "+json.orders[i].fulfillment_status)
-    		//console.log("lineitems: "+json.orders[i].line_items[0].title)
-            if(json.orders[i].customer){
-            //console.log("cust: "+JSON.stringify(json.orders[i].customer.email))
-            console.log("cust: "+json.orders[i].customer.email)
-            }
-        
-   		   //console.log(json)
-    	}
-    	
-    }
-    );*/
-
-
 //GET /admin/orders.json?since_id=667062206575
 //'https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json'
 //GET /admin/orders.json?ids=671314968687
 //https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json?ids=667073937519
 
-fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json?since_id=667062206575')
+fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18@luxarity-test.myshopify.com/admin/orders.json?ids=667062206575')
     .then(res => res.json())
     .then(json => {
 
-        console.log("looping through orders")
-    	for(var i = 0; i < json.orders.length; i++){
+        console.log("calling jsonloop ")
+        jsonLoop(json);
+    	
+    }
+    );
+
+
+async function jsonLoop(json){
+  console.log("looping through orders")
+      for(var i = 0; i < json.orders.length; i++){
             
-            //var order = [];
-            //orderid
-            //customer.email
-            //total_price
-            //order_number
-            /*if(json.orders[i].customer){
-            console.log(json.orders[i].customer.email)
-            }else{
-                 console.log(json.orders[i].id)
-            }*/
 
             if(!(json.orders[i].id && json.orders[i].total_price && json.orders[i].order_number && json.orders[i].customer)){
                 //place holder for std out - possibly dlq
@@ -69,25 +41,11 @@ fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18
                 console.log("send this order id to sqs queue: "+json.orders[i].id + " "+json.orders[i].customer.email)
                 try{
 
-                //console.log("orderId: "+json.orders[i].id);
-                //console.log("orderNumber: "+json.orders[i].order_number);
-                
-                ////var redemptionPin = json.orders[i].id.toString()+json.orders[i].order_number.toString();
-                
-                //console.log("remptionPin: "+redemptionPin);
-                
-                ////var redemptionPin256 = utils.solidityKeccak256(['string'], [redemptionPin])
-                ////var customerEmail256 = utils.solidityKeccak256(['string'], [json.orders[i].customer.email])
-                
-                //console.log("redemptionPin256: "+redemptionPin256)
-                //console.log("customerEmail256: "+customerEmail256)
-                //json.orders[i].customer.email
-
                 //sendsqs(json.orders[i].id, json.orders[i].total_price, json.orders[i].order_number, customerEmail256, redemptionPin256)
                 try{
                 console.log("sendIpfs before");
-                sendIpfs(json.orders[i].customer.email, json.orders[i].id, json.orders[i].total_price, json.orders[i].order_number);
-                console.log("sendIpfs after");
+                var testurl = await sendIpfs(json.orders[i].customer.email, json.orders[i].id, json.orders[i].total_price, json.orders[i].order_number);
+                console.log("sendIpfs after with testurl: "+testurl);
                 }catch(err){
                     throw new Error(err)
                 }
@@ -99,12 +57,9 @@ fetch('https://a2cb93368c968029e8ffdf925ab3f4f6:10d3c4d7bef29c260283452aad702c18
 
             }
 
-    		
-    	}
-    	
-    }
-    );
-
+        
+      }
+}
 //add regular customer_email
 async function sendsqs(tokenUri, orderid, total_price, order_number, customer_email_sha256, redemption_pin_sha256){
 	var params = {
@@ -149,8 +104,11 @@ async function sendIpfs(buyerId, orderId, totalPrice, orderNumber){
     console.log("Waiting for IPFS URL to be generated.");
     //setting up IPFS node
     let ipfsHash;
+    let ipfsURL;
     const node = new IPFS();
+    try{
     await node.on('ready', async () => {
+      console.log("node on ready")
       const version = await node.version()
 
       console.log('Version:', version.version);
@@ -167,11 +125,11 @@ async function sendIpfs(buyerId, orderId, totalPrice, orderNumber){
 
       console.log('Added file contents:', fileBuffer.toString());
 
-      let ipfsURL = "https://ipfs.io/ipfs/" + ipfsHash;
+      //let ipfsURL = "https://ipfs.io/ipfs/" + ipfsHash;
+      ipfsURL = "https://ipfs.io/ipfs/" + ipfsHash;
       console.log("ipfsURL: "+ipfsURL)
-      node.stop(() => {
-            console.log("node is now offline")
-        })
+
+      
       //return ipfsURL;
 
       var redemptionPin = orderId.toString()+orderNumber.toString();
@@ -179,10 +137,19 @@ async function sendIpfs(buyerId, orderId, totalPrice, orderNumber){
       var redemptionPin256 = utils.solidityKeccak256(['string'], [redemptionPin])
       var customerEmail256 = utils.solidityKeccak256(['string'], [buyerId])
 
-      await sendsqs(ipfsURL, orderId, totalPrice, orderNumber, customerEmail256, redemptionPin256);
+      //await sendsqs(ipfsURL, orderId, totalPrice, orderNumber, customerEmail256, redemptionPin256);
       console.log("done");
+       node.stop(() => {
+    // node is now 'offline'\
+      console.log("stopping ipfs")
+      })
+      return ipfsURL;
+      
 
-    });
+    })
+  }catch(err){console.log("ipfs node on ready error: "+err)}
+
+    //return ipfsURL;
 }
 
 
